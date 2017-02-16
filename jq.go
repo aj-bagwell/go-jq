8f44dcf5d2a33e89fee0ce6129f2bb6c0635cc7a
wordi <wordijp@gmail.com>
2017-02-16 10:01:19 +0900

    use original repository
    equal Fix README formatting 69ece0f73dafd875dc7869a789993420f2227230

--- jq.go
+++ jq.go
@@ -11,15 +11,14 @@ import (
 )
 
 type JQ struct {
-	program    string
-	state      *C.jq_state
-	lastValue  C.jv
-	prevRefCnt int
+	program   string
+	state     *C.jq_state
+	lastValue C.jv
 }
 
 func NewJQ(program string) (*JQ, error) {
 	state := C.jq_init()
-	jq := &JQ{program, state, C.jv_invalid(), 0}
+	jq := &JQ{program, state, C.jv_invalid()}
 	if err := jq.compile(program); err != nil {
 		jq.Close()
 		return nil, err
@@ -36,12 +35,9 @@ func (jq *JQ) HandleJson(text string) {
 }
 
 func (jq *JQ) Next() bool {
-	// jq.ValueJson() is self execute internally (jv_dump_term in the jv_dump_string)
-	if jq.prevRefCnt == jq.jv_get_refcnt() {
-		freeJv(jq.lastValue)
-	}
+	// FIXME this raises assertion if called before start()
+	freeJv(jq.lastValue)
 	jq.lastValue = jq.next()
-	jq.prevRefCnt = jq.jv_get_refcnt()
 	return isValid(jq.lastValue)
 }
 
@@ -75,10 +71,6 @@ func (jq *JQ) next() C.jv {
 
 func (jq *JQ) teardown() {
 	C.jq_teardown(&jq.state)
-}
-
-func (jq *JQ) jv_get_refcnt() int {
-	return (int)(C.jv_get_refcnt(jq.lastValue))
 }
 
 // JSON values
